e895868ce1800b62cd1a9befdc8c79d576e31c87
thibault brocherieux <l3info005@ic2s122-11.univ-lemans.fr>
2017-10-18 12:03:15 +0200

    MAJ

--- emetteur.c
+++ emetteur.c
@@ -57,3 +57,55 @@ void affichageMatriceH( int * matriceH, const int nbUtilisateurs ){
 	printf("\n\n");
 }
 
+
+/* Retourne la longueur d'un message (Marqueur de fin exclut)
+ * @param : 
+ *   const int message * : tableau d'entier contenant le message 
+ */
+int messageLen(const int message *){
+  int i = 1;
+  int compteur = 0;
+  int valeur = message[0];
+  
+  /* tant que le delimiteur de fin n'est pas atteint */
+  while (valeur != 0){
+    valeur = message[i];
+    i++;
+  }
+  /* i - 1 car i est offset de 1 */
+  return i - 1
+} 
+
+
+/* Retourne la trame message qui va etre envoye dans le canal
+ * @param : 
+ *   const int * matriceH : Sequence d'etalement 
+ *   const int message * : tableau de longueur nbUtilisateurs  contenant un message dans chacunes de ses cases 
+ *   const int nbUtilisateurs : le nombre d'utilisateurs
+ */
+int * messageAEnvoyer(const int * matriceH, const int * message, const int nbUtilisateurs ){
+  
+  int i;
+  int valeur = message[i][0];
+  int chips[nbUtilisateurs];
+  /* On sait que le message retour contient des chips de longueurs nbUtilisateurs
+     Et qu'il y a autant de chips que la longueur du plus grand message */
+  int * messageRetour = NULL;
+  
+  if( ( messageRetour = malloc(sizeof(int) * nbUtilisateurs * messageLen(message[0]) ) ) == NULL ){
+    printf("Debordement memoire\n");
+    return NULL;
+  }
+  else{
+    /* on repete la boucle pour tout les utilisateurs */
+    for(i = 0; i < nbUtilisateurs; i++){
+    
+      /* on repete la boucle pour chaque caractere du message */
+      j = 0
+      while( valeur != 0 ){
+     
+        /* on va chercher la sequence d'etalement de l'utilisateur specifie */
+        for(k = 0; k < nbUtilistateurs; k++){
+          /* messageRetour[j*k] permet de pointer vers le chip numero j et d'indice k */
+          messageRetour[j*k] = (valeur * matriceH[i + k * nbUtilisateurs]) + messageRetour[j*k];
+
