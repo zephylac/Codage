d53c3f96a9a8a5b4a5baf152c69211a1f7bd3d1e
Brocherieux Thibault <l3info005@ic2s130-07.univ-lemans.fr>
2017-11-14 09:43:53 +0100

    fix

--- recepteur.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int * recevoirMessage(const int nbUtilisateurs, const int len );
-
-int * desetalementMessage( const int * matriceH, const int * message, const int nbUtilisateurs, const int len );
-
-void afficheMessageRecu( const int * messageDesetale, const int nbUtilisateur, const int len );
