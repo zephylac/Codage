bd816781333654dfb5a9330de6dc6d97ce54457a
Brocherieux Thibault <l3info005@ic2s130-07.univ-lemans.fr>
2017-11-14 09:41:11 +0100

    :running:

---
+++ pseudo_aleatoire/gen_LM.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "gen_LM.h"
+
+
+lm_t initialiser(void){
+  lm_t codeur;
+	
+	printf(" Saisir la longueur de la sequence a generer\n");
+	scanf("%i"
---
+++ pseudo_aleatoire/gen_LM.h
@@ -0,0 +1,8 @@
+
+
+typedef struct lm_s{
+	int taille,
+  int * polynome,
+	int * initialisation;
+} lm_t;
+
